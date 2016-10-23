d457294f7d3bbb5e2451863ac2021cd93c42de03
teesloane <tylersloane@gmail.com>
2016-10-23 00:27:45 -0400

    Apply basic Local storage methods.
    Currently only save to local storage has been implemented.
    WIP: need to
    - fetch data and try displaying it
    - setup a basic auto save
    - display actual content in the editor

--- public/main.js
+++ public/main.js
@@ -1,17 +1,54 @@
 /** Husk's Javascript Goop.
 * Contents
+  * Globals
   * Vue Objects
 */
 
+/*************************
+- - - Globals - - -
+ **************************/
+
+// vars
+var g = {
+  storageKey: 'husk_user_storage',
+}
+
+// LocalStorage
+var db = {
+  fetch: function() {
+    return JSON.parse(localStorage.getItem(g.storageKey || ''))
+  },
+
+  save: function(payload) {
+    console.log(payload)
+    localStorage.setItem(g.storageKey, JSON.stringify(payload))
+  },
+
+}
 
 /*************************
-- - - A. Vue Objects - - -
+- -  Vue Instance - -
 **************************/
 
 // Vue Objects
 var App = new Vue ({
   el: '#App',
-  data: {
 
+  data: {
+    db: {
+      userSettings: null,
+      editorContents: '',
+    },
+  },
+
+  methods: {
+    save: db.save
+  },
+
+  created: function() {
+    console.log('vue instance created')
+    // TODO: create setInterval to autosave content. 
   }
+
 })
+
